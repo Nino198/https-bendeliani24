38d3eeca42155593320d4560dcf658dbc5d63f77
Ninosvani22 <151477098+Nino198@users.noreply.github.com>
2024-10-13 16:26:02 +0400

    დავალება2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//
+let userName =[1,2,4,-3, 8,-5,18,12]
+for (let userName = 0; iuserName < array.length; index++) {
+    const element = array[item[2]];
+    console.log('item'[1]);
+    for (let userName = 0; userName < array.length; userName++) {
+        const userName = array[userName];
+        console.log('itrm'[3])
+    }
+}
+let userName2 =[1,2,3,4]
+
+let result=userName.langth;
+
+
